1449741b69354a08a30994e05586cdb6d3e36ee7
GaozyDev <670453367@qq.com>
2022-04-08 21:03:01 +0800

    props配置

---
+++ 03_src_props配置/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Student name="张三" sex="男" :age="18"/>
+  </div>
+</template>
+
+<script>
+import Student from './components/Student'
+
+export default {
+  name: 'App',
+  components: {
+    Student
+  }
+}
+</script>
