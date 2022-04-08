52d7e5eb9b38c11c150e5131febf26ae62ac41b6
GaozyDev <670453367@qq.com>
2022-04-08 21:05:59 +0800

    mixin混合

---
+++ 04_src_mixin混入（合）/src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+import {mixin,mixin2} from "./mixin"
+
+Vue.config.productionTip = false
+
+Vue.mixin(mixin)
+Vue.mixin(mixin2)
+
+new Vue({
+  el:'#App',
+  render: h => h(App),
+})
