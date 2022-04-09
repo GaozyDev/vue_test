c33962f4d4fc486b8df51f6897bdda465dc8de09
GaozyDev <670453367@qq.com>
2022-04-09 22:36:33 +0800

    scoped样式

---
+++ 06_src_scoped样式/src/components/Student.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="demo">
+    <h2>学生姓名：{{name}}</h2>
+    <h2 class="test">学生性别：{{sex}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Student',
+  data() {
+    return {
+      name:'张三',
+      sex:'男'
+    }
+  }
+}
+</script>
+
+//此处不写lang="less"的意思，默认就是CSS,,scoped意思是让样式在局部生效
+<style lang="less" scoped>
+  .demo {
+    background-color: pink;
+    .test{
+      font-size: 40px;
+    }
+  }
+</style>
+
