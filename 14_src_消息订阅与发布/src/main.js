eefe71f8c8b401eb5cb4d2050cf0472ad8745c39
GaozyDev <670453367@qq.com>
2022-04-10 20:53:10 +0800

    消息订阅与发布

---
+++ 14_src_消息订阅与发布/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+// const Demo = Vue.extend({})
+// //此处的d是vc
+// const d = new Demo()
+
+
+new Vue({
+  el:'#app',
+  render: h => h(App),
+  // beforeCreate() {
+  //   Vue.prototype.$bus = this //安装全局事件总线
+  // }
+})
