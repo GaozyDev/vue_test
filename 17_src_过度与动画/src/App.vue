b8f5231bbbc34ca171bfad0d990fec09a1d0ac99
GaozyDev <670453367@qq.com>
2022-04-11 23:03:00 +0800

    过度与动画

--- 17_src_过度与动画/src/App.vue
+++ 17_src_过度与动画/src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Test/>
+    <Test2/>
+    <Test3/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test'
+import Test2 from './components/Test2'
+import Test3 from './components/Test3'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Test,
+    Test2,
+    Test3
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ 17_src_过度与动画/src/components/Test3.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+<!--    添加appear属性，实现入场过渡：当元素加载的时候，执行动画-->
+    <transition-group
+        appear
+        name="animate__animated animate__bounce"
+        enter-active-class="animate__swing"
+        leave-active-class="animate__backOutUp"
+    >
+      <h1 v-show="!isShow" class="come" key="1">你好啊!</h1>
+      <h1 v-show="isShow" class="come" key="2">尚硅谷</h1>
+    </transition-group>
+  </div>
+</template>
+
+<script>
+import 'animate.css'
+export default {
+  name: 'Test3',
+  data() {
+    return {
+      isShow: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  background-color: orange;
+}
+</style>
