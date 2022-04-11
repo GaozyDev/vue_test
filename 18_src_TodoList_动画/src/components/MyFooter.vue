a2b07a984d0dbf34cc9bcedb55376a15dd37428e
GaozyDev <670453367@qq.com>
2022-04-12 00:00:43 +0800

    TodoList_nextTick

---
+++ 18_src_TodoList_动画/src/components/MyFooter.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="todo-footer" v-show="total">
+    <label>
+      <!--<input type="checkbox" :checked="isAll" @change="checkAll"/>-->
+      <input type="checkbox" v-model="isAll"/>
+    </label>
+    <span>
+          <span>已完成{{ doneTotal }}</span> / 全部{{ total }}
+        </span>
+    <button class="btn btn-danger" @click="clearAll">清除已完成任务</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyFooter',
+  props: ['todos'],
+  computed: {
+    // doneTotal() {
+    //   let i = 0
+    //   this.todos.forEach((todo)=>{
+    //     if (todo.done) i++
+    //   })
+    //   return i
+    // }
+
+
+    total() {
+      return this.todos.length
+    },
+
+    doneTotal() {
+      // const x = this.todos.reduce((pre,current )=>{
+      //   console.log('@',pre,current)
+      //   return pre + (current.done ? 1 : 0)
+      // },0)
+      // console.log(x)
+      return this.todos.reduce((pre, todo) => pre + (todo.done ? 1 : 0), 0)
+    },
+
+    isAll: {
+      get() {
+        return this.doneTotal === this.total && this.total > 0
+      },
+
+      set(value) {
+        this.$emit('checkAllTodo',value)
+      }
+    },
+
+    // isAll() {
+    //   return this.doneTotal === this.total && this.total > 0
+    // }
+  },
+  methods: {
+    // checkAll(e) {
+    //   // console.log(e.target.checked)
+    //   this.checkAllTodo(e.target.checked)
+    // },
+    clearAll() {
+      // this.clearAllTodo()
+      this.$emit('clearAllTodo')
+    },
+  }
+}
+</script>
+
+<style scoped>
+/*footer*/
+.todo-footer {
+  height: 40px;
+  line-height: 40px;
+  padding-left: 6px;
+  margin-top: 5px;
+}
+
+.todo-footer label {
+  display: inline-block;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.todo-footer label input {
+  position: relative;
+  top: -1px;
+  vertical-align: middle;
+  margin-right: 5px;
+}
+
+.todo-footer button {
+  float: right;
+  margin-top: 5px;
+}
+</style>
+
---
+++ 18_src_TodoList_动画/src/components/MyList.vue
@@ -0,0 +1,58 @@
+<template>
+  <ul class="todo-main">
+    <transition-group name="todo" appear>
+      <MyItem
+          v-for="todoObj in todos"
+          :key="todoObj.id"
+          :todo="todoObj"
+      />
+    </transition-group>
+  </ul>
+</template>
+
+<script>
+import MyItem from "@/components/MyItem";
+
+export default {
+  name: 'MyList',
+  components: {MyItem},
+  props: ['todos']
+}
+</script>
+
+<style scoped>
+/*main*/
+.todo-main {
+  margin-left: 0px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding: 0px;
+}
+
+.todo-empty {
+  height: 40px;
+  line-height: 40px;
+  border: 1px solid #ddd;
+  border-radius: 2px;
+  padding-left: 5px;
+  margin-top: 10px;
+}
+
+.todo-enter-active {
+  animation: todo 1s linear;
+}
+
+.todo-leave-active {
+  animation: todo 1s linear reverse;
+}
+
+@keyframes todo {
+  from {
+    transform: translateX(100%);
+  }
+  to {
+    transform: translateX(0);
+  }
+}
+</style>
+
