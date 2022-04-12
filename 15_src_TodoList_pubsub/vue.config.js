385778712648ecf33da41e0f985141ed1d113c38
GaozyDev <670453367@qq.com>
2022-04-12 10:19:03 +0800

    vue.config.js增加了入口文件的配置，没加入口文件配置也没出错

--- 15_src_TodoList_pubsub/vue.config.js
+++ 15_src_TodoList_pubsub/vue.config.js
@@ -1,5 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false
+  lintOnSave:false,
+  pages: {
+    index: {
+      //入口
+      entry: 'src/main.js'
+    }
+  }
 })
