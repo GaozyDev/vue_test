246b828b949414587e2955694d77948bf5b96b2a
GaozyDev <670453367@qq.com>
2022-04-12 11:21:08 +0800

    配置地理服务器

--- 19_src_配置代理服务器/src/App.vue
+++ 19_src_配置代理服务器/src/App.vue
@@ -1,28 +1,43 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <button @click="getStudents">获取学生信息</button>
+    <br>
+    <button @click="getCars">获取汽车信息</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods:{
+    getStudents() {
+      //then后面跟的response和error是axios.get请求发送后返回来的两个状态对象，
+      // response是成功，error是失败。response.data是请求成功返回来的数据
+      //error.message是请求失败返回来的数据
+      //   /api是请求走代理服务器的前缀，，紧跟在端口号后面，，，如果你的前缀是/atguiug，，那8080后面就写/atguigu
+      axios.get('http://localhost:8080/api/students').then(
+          response => {
+            console.log('请求成功了',response.data)
+          },
+          error => {
+            console.log('请求失败了',error.message)
+          }
+      )
+    },
+
+    getCars() {
+      axios.get('http://localhost:8080/demo/cars').then(
+          response => {
+            console.log('请求成功了',response.data)
+          },
+          error => {
+            console.log('请求失败了',error.message)
+          }
+      )
+    },
+
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- 19_src_配置代理服务器/vue.config.js
+++ 19_src_配置代理服务器/vue.config.js
@@ -1,4 +1,47 @@
-const { defineConfig } = require('@vue/cli-service')
+const {defineConfig} = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+    lintOnSave: false, //关闭语法检查
+    pages: {
+        index: {
+            //入口
+            entry: 'src/main.js'
+        }
+    },
+// //开启代理服务器(方式一)   下面的端口号是要请求的服务器的端口号
+//     devServer: {
+//         proxy: 'http://localhost:5000'
+//     },
+
+    //开启代理服务器(方式二)   下面的端口号是要请求的服务器的端口号
+    devServer: {
+        proxy: {
+            //   /api是请求前缀，有这个前缀就走代理服务器，没有这个前缀就不走代理，/api也可以换成别的名
+            //由于你把/api为前缀的请求发给了代理服务器，代理就把以/api为前缀的请求发给了5000端口，
+            //但是5000端口的服务器上并没有以/api为开头的资源，所以会请求失败。所以要加上pathRewrite:{'^/api':''}
+            //重写路径，是个对象，里面是'key':'value'，，，表示匹配所有以api开头的路径，换成以空字符串开头
+            '/api': {
+                target: 'http://localhost:5000',
+                pathRewrite:{'^/api':''},
+                ws: true, //用于支持websocket
+                changeOrigin: true  //用于控制请求头中的host值
+                //代理服务器发送请求给5000端口服务器时，5000会问你来自哪里？如果配置成true
+                //代理就会说谎说我来自于和你一样，也来自5000；
+                // 如果配置成false，代理就会如实回答说我来自于一台端口号为8080的代理服务器
+            },
+
+            '/demo': {
+                target: 'http://localhost:5001',
+                pathRewrite:{'^/demo':''},
+                ws: true,
+                changeOrigin: true
+            },
+
+            //下面是上面代码的精简版
+            // '/foo': {
+            //     target: '<other_url>'
+            // }
+        }
+    }
+
 })
