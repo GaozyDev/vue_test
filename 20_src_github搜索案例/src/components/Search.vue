56ec3aa6b82caeac7c3553af974e48a710993672
GaozyDev <670453367@qq.com>
2022-04-12 16:58:11 +0800

    github案例完善案例

--- 20_src_github搜索案例/src/components/Search.vue
+++ 20_src_github搜索案例/src/components/Search.vue
@@ -19,14 +19,20 @@ export default {
   },
   methods:{
     searchUsers() {
+      //请求前更新List的数据
+      this.$bus.$emit('updateListData',{isFirst:false,isLoading:true,errMsg:'',users:[]})
       // xxx是要搜索的数据   ${this.keyWord}是ES6的模板字符串
       axios.get(`https://api.github.com/search/users?q=${this.keyWord}`).then(
           response => {
             // console.log('请求成功了',response.data.items)
-            this.$bus.$emit('getUsers',response.data.items)
+            // this.$bus.$emit('getUsers',response.data.items)
+            //请求成功后更新List的数据
+            this.$bus.$emit('updateListData',{isLoading:false,errMsg:'',users:response.data.items})
           },
           error => {
-            console.log('请求失败了',error.message)
+            //请求失败更新List的数据
+            // console.log('请求失败了',error.message)
+            this.$bus.$emit('updateListData',{isLoading:false,errMsg:error.message,users:[]})
           }
       )
     }
