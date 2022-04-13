7d22aefbd1b1e5da5b1caa3a6105b71214f3f61e
GaozyDev <670453367@qq.com>
2022-04-13 09:44:11 +0800

    搜索案例vue-resource版本

--- 21_src_github搜索案例_vue-resource/src/main.js
+++ 21_src_github搜索案例_vue-resource/src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+//引入插件
+import vueResource from 'vue-resource'
+//使用插件
+Vue.use(vueResource)
 
 Vue.config.productionTip = false
 
 new Vue({
+  el:'#app',
   render: h => h(App),
-}).$mount('#app')
+  beforeCreate() {
+    Vue.prototype.$bus = this //安装全局事件总线
+  }
+})
