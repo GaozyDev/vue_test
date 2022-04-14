cc5c85e7a698eaa2eb4faf3878cefe60add58737
GaozyDev <670453367@qq.com>
2022-04-14 13:28:56 +0800

    多组件共享数据

---
+++ 26_src_求和案例_多组件共享数据/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Count/>
+    <hr>
+    <Persons/>
+  </div>
+</template>
+
+<script>
+import Count from './components/Count'
+import Persons from './components/Persons'
+
+export default {
+  name: 'App',
+  components: {
+    Count,
+    Persons
+  },
+  mounted() {
+    // console.log('App',this)
+  }
+}
+</script>
---
+++ 26_src_求和案例_多组件共享数据/src/main.js
@@ -0,0 +1,30 @@
+//引入Vue
+import Vue from 'vue'
+//引入App
+import App from './App.vue'
+//引入插件
+import vueResource from 'vue-resource'
+//引入store
+import store from './store'
+
+//关闭Vue的生产提示
+Vue.config.productionTip = false
+//使用插件
+Vue.use(vueResource)
+
+//创建vm
+new Vue({
+  el:'#app',
+  render: h => h(App),
+  store,
+  //入口
+  // pages: {
+  //   index: {
+  //     //入口
+  //     entry: 'src/main.js'
+  //   }
+  // }
+  beforeCreate() {
+    Vue.prototype.$bus = this
+  }
+})
