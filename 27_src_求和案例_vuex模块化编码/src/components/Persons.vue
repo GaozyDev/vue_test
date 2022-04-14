95e0a7fcefd57548ffbd6deaa97af2da8ff471f6
GaozyDev <670453367@qq.com>
2022-04-14 19:54:00 +0800

    模块化+网络请求

--- 27_src_求和案例_vuex模块化编码/src/components/Persons.vue
+++ 27_src_求和案例_vuex模块化编码/src/components/Persons.vue
@@ -5,6 +5,8 @@
     <h3>列表中第一个人的名字是：{{firstPersonName}}</h3>
     <input type="text" placeholder="请输入名字" v-model="name">
     <button @click="add">添加</button>
+    <button @click="addWang">添加一个姓王的人</button>
+    <button @click="addPersonServer">添加一个人,名字随机</button>
     <ul>
       <li v-for="p in personList" :key="p.id">{{ p.name }}</li>
     </ul>
@@ -43,6 +45,14 @@ export default {
       // console.log(personObj)
       this.$store.commit('personAbout/ADD_PERSON',personObj)
       this.name = ''
+    },
+    addWang() {
+      const personObj = {id:nanoid(),name:this.name}
+      this.$store.dispatch('personAbout/addPersonWang',personObj)
+      this.name = ''
+    },
+    addPersonServer() {
+      this.$store.dispatch('personAbout/addPersonServer')
     }
   }
 }
