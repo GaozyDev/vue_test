66e48f4bb6e06b82ef768b535a0fc52f5e28f06b
GaozyDev <670453367@qq.com>
2022-04-15 23:34:51 +0800

    全局路由守卫

--- 29_src_路由/src/router/index.js
+++ 29_src_路由/src/router/index.js
@@ -13,27 +13,32 @@ const router =  new VueRouter({
         {
             name:'guanyu',
             path:'/about',
-            component:About
+            component:About,
+            meta:{title:'关于'},
         },
         {
             name:'zhuye',
             path:'/home',
             component:Home,
+            meta:{title:'主页'},
             children:[
                 {
                     name:'xinwen',
                     path:'news',
-                    component:News
+                    component:News,
+                    meta:{isAuth:true,title:'新闻'}
                 },
                 {
                     name:'xiaoxi',
                     path:'message',
                     component:Message,
+                    meta:{isAuth:true,title:'消息'},
                     children:[
                         {
                             name:'xiangqing',
                             path:'detail',
                             component:Detail,
+                            meta:{title:'详情'},
 
                             // //props的第一种写法，值为对象，该对象中的福哦有key-value都会以props的形式传给Detail组件
                             // props:{a:1,b:'hello'}
@@ -62,15 +67,22 @@ const router =  new VueRouter({
 
 //全局前置路由守卫----初始化的时候被调用、每次路由切换之前被调用
 router.beforeEach((to,from,next) =>{
-    if(to.path === '/home/news' || to.path === '/home/message') {
+    if(to.meta.isAuth) {//判断是否需要鉴权
         if(localStorage.getItem('school')==='atguigu') {
+            // document.title = to.meta.title || '硅谷系统'
             next()
         }else{
             alert('学校名不对，无权限！！')
         }
     }else{
+        // document.title = to.meta.title || '硅谷系统'
         next()
     }
+})
+
+//全局后置路由守卫----初始化的时候被调用、每次路由切换之后被调用
+router.afterEach((to,from) =>{
+    document.title = to.meta.title || '硅谷系统'
 })
 
 export default router
