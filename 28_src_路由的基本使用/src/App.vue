a2126ce7242760d0f51008fe134d03b1bf7e3499
GaozyDev <670453367@qq.com>
2022-04-15 10:32:55 +0800

    路由的基本使用

--- 28_src_路由的基本使用/src/App.vue
+++ 28_src_路由的基本使用/src/App.vue
@@ -8,14 +8,20 @@
     <div class="row">
       <div class="col-xs-2 col-xs-offset-2">
         <div class="list-group">
-          <a class="list-group-item active" href="./about.html">About</a>
-          <a class="list-group-item" href="./home.html">Home</a>
+          <!--原始html中我们使用a标签实现页面的跳转-->
+          <!--<a class="list-group-item active" href="./about.html">About</a>-->
+          <!--<a class="list-group-item" href="./home.html">Home</a>-->
+
+          <!--Vue中借助router-link标签实现路由的切换-->
+          <router-link class="list-group-item" active-class="active" to="/about">About</router-link>
+          <router-link class="list-group-item" active-class="active" to="/home">Home</router-link>
         </div>
       </div>
       <div class="col-xs-6">
         <div class="panel">
           <div class="panel-body">
-            <h2>我是About的内容</h2>
+<!--            指定组件的呈现位置-->
+            <router-view></router-view>
           </div>
         </div>
       </div>
@@ -24,11 +30,11 @@
 </template>
 
 <script>
+import Home from './components/Home'
+import About from './components/About'
 
 export default {
   name: 'App',
-  mounted() {
-    // console.log('App',this)
-  }
+  components:{About,Home},
 }
 </script>
--- 28_src_路由的基本使用/src/main.js
+++ 28_src_路由的基本使用/src/main.js
@@ -2,21 +2,19 @@
 import Vue from 'vue'
 //引入App
 import App from './App.vue'
+//引入VueRouter
+import VueRouter from 'vue-router'
+//引入路由器
+import router from './router'
 
+//应用插件
+Vue.use(VueRouter)
 
-//关闭Vue的生产提示
+//关闭生产提示
 Vue.config.productionTip = false
 
-
-//创建vm
 new Vue({
   el:'#app',
   render: h => h(App),
-  //入口
-  // pages: {
-  //   index: {
-  //     //入口
-  //     entry: 'src/main.js'
-  //   }
-  // }
+  router,
 })
