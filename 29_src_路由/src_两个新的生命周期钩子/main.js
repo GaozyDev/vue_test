f8c0f62afd2c9a3c6fb903b6d954cae7fb1624a1
GaozyDev <670453367@qq.com>
2022-04-15 17:03:07 +0800

    路由独有的两个生命周期钩子

---
+++ 29_src_路由/src_两个新的生命周期钩子/main.js
@@ -0,0 +1,20 @@
+//引入Vue
+import Vue from 'vue'
+//引入App
+import App from './App.vue'
+//引入VueRouter
+import VueRouter from 'vue-router'
+//引入路由器
+import router from './router'
+
+//应用插件
+Vue.use(VueRouter)
+
+//关闭生产提示
+Vue.config.productionTip = false
+
+new Vue({
+  el:'#app',
+  render: h => h(App),
+  router,
+})
